e28f429d9270311d70156e2227e53e5c0a551da7
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2022-05-02 16:22:11 +0300

    Refactor code

--- src/load_image.py
+++ src/load_image.py
@@ -1,10 +1,10 @@
-import pygame
 import os
+import pygame
 
 dirname = os.path.dirname(__file__)
 
 
 def load_image(filename):
     return pygame.image.load(
-        os.path.join(dirname, "assets", filename)
+        os.path.join(dirname, "images", filename)
     )
